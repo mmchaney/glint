113949a3254e3dfe6ce6dd1a65b6702cdc51dbf0
mmchaney <markusmc@gmail.com>
2010-12-06 14:03:29 +0100

    Don't show spinner on waiting.

--- src/player.js
+++ src/player.js
@@ -109,7 +109,7 @@
 
     this.activateLoading = this.setLoading.bind(this, true);
     this.video.addEventListener('loadstart', this.setLoading.bind(this, true, 'loadstart'), false);
-    this.video.addEventListener('waiting', this.setLoading.bind(this, true, 'waiting'), false);  
+    // this.video.addEventListener('waiting', this.setLoading.bind(this, true, 'waiting'), false);  
     this.video.addEventListener('stalled', this.setLoading.bind(this, true, 'stalled'), false);    
 
     this.deactivateLoading = this.setLoading.bind(this, false);    
