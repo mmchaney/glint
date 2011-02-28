3ed7dea48b80129624165c792319d3c54f3ddf81
U-Markus-PC\Markus <Markus@Markus-PC.(none)>
2011-02-28 14:39:04 +0100

    Switched to JSHint

--- src/glint.js
+++ src/glint.js
@@ -1,12 +1,12 @@
 /*
  * Glint: A tiny HTML5 video player for modern browsers
- * 2010-11-21
  *
  * By Markus Messner-Chaney, http://markusmessnerchaney.com
  * Public Domain.
  * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
  */
  
-/*jslint white: true, browser: true, devel: true, windows: true, onevar: true, undef: true, eqeqeq: true, 
-bitwise: true, regexp: true, newcap: true, immed: true, maxlen: 120, indent: 2 */
+/*jshint browser: true, boss: true, curly: true, eqeqeq: true, immed: true, 
+  newcap: true, noarg: true, noempty: true, overvar: true, undef: true, 
+  white: true, indent: 2 */
 /*global glint, Element, Function, Object */
